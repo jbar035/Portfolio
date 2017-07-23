99f4acaac3ff218ddfddd808083170b2c55ed62c
jbar035 <jake.barnes1@hotmail.co.uk>
2017-07-24 00:00:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 
+function main() {
+  $('.navbar navbar-inverse').hide();
+  $('.navbar navbar-inverse').fadeIn(1000);
+  
+ // $('.projects').hide();
+  
+ // $('.projects-button').on('click', function() {
+		// $(this).next().toggle();
+  //  $(this).next().slideToggle(4000);
+  //  $(this).toggleClass('active');
+   // $(this).text('Projects Viewed');
+	});
+}
+
+$(document).ready(main);
