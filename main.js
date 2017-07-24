2037665e31e3cce314898da89f072349cd93dfbf
Jake Barnes <jake.barnes1@hotmail.co.uk>
2017-07-24 17:23:03 +0100

    Added more pages

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
 
 function main() {
-//  $('.navbar navbar-inverse').hide();
-//  $('.navbar navbar-inverse').fadeIn(1000);
+
  
-  $('#wrapper').hide();
-  $('#wrapper').fadeIn(3000);
+  // $('#wrapper').hide();
+  // $('#wrapper').fadeIn(3000);
 
+    // $('#myNavbar').click(function(){
+    //     // $("#div1").fadeOut();
+    //     // $("#div2").fadeOut("slow");
+        
+    //     $('#wrapper').fadeOut(2000);
 
+    // });
 
-
-
- // $('.projects').hide();
-  
- // $('.projects-button').on('click', function() {
-		// $(this).next().toggle();
-  //  $(this).next().slideToggle(4000);
-  //  $(this).toggleClass('active');
-   // $(this).text('Projects Viewed');
-//	});
 }
 
 
 $(document).ready(main);
 
-$(document).ready(function() {
-   $('#myNavbar').on('click', function() {
-     $('#overlay').animate({
-       opacity: 1,
-     }, 5000, function() {
-        // Animation complete.
-     });
-   }); 
-});
+
