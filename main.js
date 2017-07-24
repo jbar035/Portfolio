5af3b7fe58a4dc3af3a37b1df263a265242a61d5
Jake Barnes <jake.barnes1@hotmail.co.uk>
2017-07-24 01:23:57 +0100

    Update1am
    random testing

--- main.js
+++ main.js
@@ -2,7 +2,14 @@
 function main() {
 //  $('.navbar navbar-inverse').hide();
 //  $('.navbar navbar-inverse').fadeIn(1000);
-  alert("TEST");
+ 
+  $('#wrapper').hide();
+  $('#wrapper').fadeIn(3000);
+
+
+
+
+
  // $('.projects').hide();
   
  // $('.projects-button').on('click', function() {
@@ -13,4 +20,15 @@ function main() {
 //	});
 }
 
+
 $(document).ready(main);
+
+$(document).ready(function() {
+   $('#myNavbar').on('click', function() {
+     $('#overlay').animate({
+       opacity: 1,
+     }, 5000, function() {
+        // Animation complete.
+     });
+   }); 
+});
