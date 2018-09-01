0b8fc2d53f6f8f063de276312f2d30485973f254
Jake Barnes <jake@jakes-mbp.home>
2018-09-01 14:12:52 +0100

    Updated Github tutorial

--- javascript/websiteFunctions.js
+++ javascript/websiteFunctions.js
@@ -1,3 +1,12 @@
 function getDate() {
   document.write(new Date().getFullYear());
 }
+//change to pass id
+function toggleContent() {
+  var x = document.getElementById("gitExampleContent");
+  if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
